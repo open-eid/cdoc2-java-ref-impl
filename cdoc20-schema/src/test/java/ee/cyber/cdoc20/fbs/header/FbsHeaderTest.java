4511ce088513395571b82f521248de7b40494aa6
Janno Kusman <janno.kusman@cyber.ee>
2022-03-07 12:10:05 +0200

    Initial project POMs, flatbuffers compiling

---
+++ cdoc20-schema/src/test/java/ee/cyber/cdoc20/fbs/header/FbsHeaderTest.java
@@ -0,0 +1,135 @@
+package ee.cyber.cdoc20.fbs.header;
+
+import ee.cyber.cdoc20.fbs.recipients.ECCPublicKey;
+import ee.cyber.cdoc20.fbs.recipients.EllipticCurve;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.google.flatbuffers.FlatBufferBuilder;
+
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+//import java.nio.channels.FileChannel;
+import java.util.Arrays;
+
+public class FbsHeaderTest {
+
+    //public static final int KEYLEN_BITS = 256;
+    public static final int KEYLEN_BYTES = 256 / 8;
+
+    @Test
+    void testFbsHeaderSerialization() throws IOException {
+
+        byte payloadEnc = PayloadEncryptionMethod.CHACHA20POLY1305;
+
+        byte[] fmkBuf =  new byte[KEYLEN_BYTES];
+        //Arrays.fill(fmkBuf, (byte) 0x00);
+        fmkBuf[0] = 'f';
+        fmkBuf[1] = 'm';
+        fmkBuf[2] = 'k';
+        fmkBuf[fmkBuf.length - 1] = (byte)0xff;
+
+
+        byte[] recipientPubKeyBuf = new byte[KEYLEN_BYTES];
+        //Arrays.fill(recipientPubKeyBuf, (byte) 0x00);
+        recipientPubKeyBuf[0] = 'r';
+        recipientPubKeyBuf[1] = 'e';
+        recipientPubKeyBuf[2] = 'c';
+        recipientPubKeyBuf[recipientPubKeyBuf.length - 1] = (byte)0xfe;
+
+
+
+        byte[] senderPubKeyBuf = new byte[KEYLEN_BYTES];
+        //Arrays.fill(senderPubKeyBuf, (byte) 0x00);
+        senderPubKeyBuf[0] = 's';
+        senderPubKeyBuf[1] = 'e';
+        senderPubKeyBuf[2] = 'n';
+        senderPubKeyBuf[senderPubKeyBuf.length - 1]  = (byte)0xfc;
+
+
+
+
+
+
+        FlatBufferBuilder builder = new FlatBufferBuilder(1024);
+
+
+        int recipientPubKeyOffset = builder.createByteVector(recipientPubKeyBuf);
+        int senderPubKeyOffset = builder.createByteVector(senderPubKeyBuf);
+        int eccPubKeyOffset = ECCPublicKey.createECCPublicKey(builder,
+                EllipticCurve.secp384r1,
+                recipientPubKeyOffset,
+                senderPubKeyOffset
+        );
+
+        int encFmkOffset = RecipientRecord.createEncryptedFmkVector(builder, fmkBuf);
+
+        RecipientRecord.startRecipientRecord(builder);
+        RecipientRecord.addDetailsType(builder, Details.recipients_ECCPublicKey);
+        RecipientRecord.addDetails(builder, eccPubKeyOffset);
+
+        RecipientRecord.addEncryptedFmk(builder, encFmkOffset);
+        RecipientRecord.addFmkEncryptionMethod(builder, FMKEncryptionMethod.XOR);
+
+
+        // endRecipientRecord will return RecipientRecord offset value
+        int[] recipients = new int[] {RecipientRecord.endRecipientRecord(builder)};
+        int recipientsOffset = Header.createRecipientsVector(builder, recipients);
+
+        Header.startHeader(builder);
+        Header.addRecipients(builder, recipientsOffset);
+        Header.addPayloadEncryptionMethod(builder, payloadEnc);
+        int headerOffset = Header.endHeader(builder);
+        Header.finishHeaderBuffer(builder, headerOffset);
+
+        ByteBuffer buf = builder.dataBuffer();
+
+// temp examples generation
+//        FileChannel fc = new FileOutputStream("target/Header.bin").getChannel();
+//        int pos = buf.position();
+//        int limit = buf.limit();
+//        fc.write(buf);
+//        fc.close();
+//
+//        buf.position(pos);
+//        buf.limit(limit);
+
+        Header header = Header.getRootAsHeader(buf);
+
+        Assertions.assertNotNull(header);
+
+        Assertions.assertEquals(payloadEnc, header.payloadEncryptionMethod());
+
+        Assertions.assertEquals(1, header.recipientsLength());
+        RecipientRecord recipient = header.recipients(0);
+
+
+        Assertions.assertNotNull(recipient.encryptedFmkAsByteBuffer());
+        Assertions.assertEquals(fmkBuf.length, recipient.encryptedFmkLength());
+
+
+        Assertions.assertEquals(fmkBuf[0], (byte)recipient.encryptedFmk(0));
+
+
+
+        //whole underlying bytebuffer, with position() set to start of fmkBuf and limit() at end of fmkBuf
+        ByteBuffer byteBuffer = recipient.encryptedFmkAsByteBuffer();
+
+        byte[] fmkArray = Arrays.copyOfRange(byteBuffer.array(), byteBuffer.position(), byteBuffer.limit());
+
+        //preferred for speed
+        byte[] fmkBytes = new byte[KEYLEN_BYTES];
+        byteBuffer.get(byteBuffer.position(), fmkBytes);
+
+        Assertions.assertArrayEquals(fmkBuf, fmkArray);
+        Assertions.assertArrayEquals(fmkBuf, fmkBytes);
+        Assertions.assertEquals(ByteBuffer.wrap(fmkBuf), byteBuffer.slice());
+
+        //TODO: check recipients
+
+
+
+    }
+}
