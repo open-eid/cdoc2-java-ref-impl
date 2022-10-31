8ba312707d073042f4143303b0fab0a9926e6a67
Rainer Metsvahi <rainer@cyber.ee>
2022-10-31 16:04:52 +0200

    Remove spring security from put-server

--- cdoc20-server/put-server/src/main/java/ee/cyber/cdoc20/server/Cdoc20PutServerApplication.java
+++ cdoc20-server/put-server/src/main/java/ee/cyber/cdoc20/server/Cdoc20PutServerApplication.java
@@ -1,31 +1,24 @@
 package ee.cyber.cdoc20.server;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @SpringBootApplication
 @Configuration
-@EnableWebSecurity
 @EnableJpaAuditing
 @Slf4j
-public class Cdoc20PutServerApplication extends WebSecurityConfigurerAdapter {
+public class Cdoc20PutServerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Cdoc20PutServerApplication.class, args);
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
-            .and()
-                .csrf().disable();
+    public void run(final String... args) throws Exception {
+        log.info("CDOC 2.0 key capsule put-server is running.");
     }
 }
