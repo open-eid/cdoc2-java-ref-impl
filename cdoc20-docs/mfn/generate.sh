6e1ce26473e85edf50d5263c5b4b33c02d876a85
Janno Kusman <janno.kusman@cyber.ee>
2024-03-27 14:46:43 +0200

    RM-2966: remove/move documents. Translate useful information

--- cdoc20-docs/mfn/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if ! [ -x "$(command -v jekyll)" ]; then
-    echo "jekyll must be installed!"
-    exit
-fi
-
-jekyll b -s jekyll -d jekyll/_site && mv jekyll/_site/index.html ./MFN.md
