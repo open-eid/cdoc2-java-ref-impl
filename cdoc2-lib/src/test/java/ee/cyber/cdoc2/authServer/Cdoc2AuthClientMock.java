31243c1eb58f3ead46e030180a27cf0e44f9e839
Hain Zuppur <hain.zuppur@cyber.ee>
2026-07-02 10:51:02 +0300

    CDOC-190: Add additional unit tests

--- cdoc2-lib/src/test/java/ee/cyber/cdoc2/authServer/Cdoc2AuthClientMock.java
+++ cdoc2-lib/src/test/java/ee/cyber/cdoc2/authServer/Cdoc2AuthClientMock.java
@@ -33,6 +33,8 @@ public class Cdoc2AuthClientMock {
     @SuppressWarnings("checkstyle:LineLength")
     public static final String SID_SIGNING_CERTIFICATE_BASE64URL = "MIIGpzCCBi6gAwIBAgIQGcJUbe6JHI6jJyV-42vjnTAKBggqhkjOPQQDAzBxMSwwKgYDVQQDDCNURVNUIG9mIFNLIElEIFNvbHV0aW9ucyBFSUQtUSAyMDI0RTEXMBUGA1UEYQwOTlRSRUUtMTA3NDcwMTMxGzAZBgNVBAoMElNLIElEIFNvbHV0aW9ucyBBUzELMAkGA1UEBhMCRUUwHhcNMjYwMTA2MTQyNTAxWhcNMjkwMTA1MTQyNTAwWjBXMQswCQYDVQQGEwJFRTEQMA4GA1UEAwwHVEVTVCxPSzENMAsGA1UEBAwEVEVTVDELMAkGA1UEKgwCT0sxGjAYBgNVBAUTEVBOT0VFLTQwNTA0MDQwMDAxMIIDIjANBgkqhkiG9w0BAQEFAAOCAw8AMIIDCgKCAwEAkI98VzyaeSueyaUQYIXMMf-1VY10Gw-b8Q13Rb9N62ROZY97wMIB__f8_PuOIoqkAPM6Tn_t4lp1R_rHrbuqs0hl2dgLlOcR5wmWmp7YfKPDvRndVLl_doIHruxY8O60rFGskSnqt4coHN4xGcmCyPkJoB8Rfm8-Y9poVKAreS0Ta32p5OSME0HjSs7-ahB2erWfb2GulFw1vyeH42d3XDpCCfd6CByvSsi4oByUqs5G-kjSrGUglflgWXK3MxBYto0swgsbD1nrW5doU_cMCfRoFURun4XguX8dTt9VeyqeJitxRfub2Hj18RbsKuoFNHQNOxAxRK4oTVCtUrYbVqBHDmoOm8r3CsSuqjuZ2njQybiUhBofpTVMCZ6lB6VgoLphmEwSEOQXIumpmpb2qJZqbZaBoyyWb4f5AQjw3Q5lwPSao5215hIgSuuENRezpP9rTzIwyOMbnV2nMSMInAuaXIXskB2NdpMsROsvOqBC0h5azTj9naCS-5EW-9eI7GGK03Du5JoKD5wYajJxfcxFwBAl8Ko71OvhGFtYiu-hqzz-CyG6NswB87KvzDYUCQ-0qOfgRBNCgYnbjnuYVJb3CGLp_cP5GmKtUC3wHX1WnPGyK4bD19Rcy-FhG6mD_ZrAPcmZ3s4FLLErpRJ3ui-fiMPLQl2bpCKTWoaEZoPg6Grnhr3bE2ZiKWmqdVwf30bG3-GnvTBTuF0T1lzt6NeBlB23SJsffCmzSFSNcFJHHYI1FYdZu2p0gL6KAabEmnE8GrTrCn93DFNBtoKu9vG30QrRzyh-itPvtn9w-9t-nDkhaVHmNCjWD1xcMeXsyK8ek0rbz5aVe_RPvCifhIpgjqNsDHh9q1QT9KIFsd6RD2XPMlekL9c6YiVY9H7uRyIQWqJwtrvNvBKj4ZT9745zTfkhCJTPvnLy-4iKeINVZ2f98BblsGAEHKGol8YA-3SRkPh9BVnVhSdI3lxCDEbmHuk21GIPE9689efSvbcDEHpqeYoxo3tXjl_hqfzPAgMBAAGjggH1MIIB8TAJBgNVHRMEAjAAMB8GA1UdIwQYMBaAFLAkFxmI42b4zShYZXtNFNiSZk9rMHAGCCsGAQUFBwEBBGQwYjAzBggrBgEFBQcwAoYnaHR0cDovL2Muc2suZWUvVEVTVF9FSUQtUV8yMDI0RS5kZXIuY3J0MCsGCCsGAQUFBzABhh9odHRwOi8vYWlhLmRlbW8uc2suZWUvZWlkcTIwMjRlMDAGA1UdEQQpMCekJTAjMSEwHwYDVQQDDBhQTk9FRS00MDUwNDA0MDAwMS1ERU0wLVEweAYDVR0gBHEwbzBjBgkrBgEEAc4fEQIwVjBUBggrBgEFBQcCARZIaHR0cHM6Ly93d3cuc2tpZHNvbHV0aW9ucy5ldS9yZXNvdXJjZXMvY2VydGlmaWNhdGlvbi1wcmFjdGljZS1zdGF0ZW1lbnQvMAgGBgQAj3oBAjAoBgNVHQkEITAfMB0GCCsGAQUFBwkBMREYDzE5MDUwNDA0MTIwMDAwWjAWBgNVHSUEDzANBgsrBgEEAYPmYgUHADA0BgNVHR8ELTArMCmgJ6AlhiNodHRwOi8vYy5zay5lZS90ZXN0X2VpZC1xXzIwMjRlLmNybDAdBgNVHQ4EFgQUX9YaVGlPdUOO2J6rzNc4sljBQBAwDgYDVR0PAQH_BAQDAgeAMAoGCCqGSM49BAMDA2cAMGQCMHhYJCeKceJv_m0xcFRssS4WVFnnCryDiuSEpjDZu0irJ_XurXXIFDr-9hhl2x7GMwIwbiD5GALRtwzUaEh-SV9jigT9Oc336f6QYf8YaSA0-Un8eRQPa9wTK0cSQrM_CUIu";
 
+    public static final int TIMEOUT_DELAY_MS = 3_000;
+
     private final WireMockExtension wiremock;
 
     public Cdoc2AuthClientMock(WireMockExtension wiremock) {
@@ -71,6 +73,120 @@ public class Cdoc2AuthClientMock {
                     {"errorCode":"AUTH_SERVER_ERROR_CODE"}
                     """
             ))
+        );
+    }
+
+    public void stubStartAuthWith400() {
+        wiremock.stubFor(
+            WireMock.post(urlEqualTo("/auth/start"))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.BAD_REQUEST_400)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"INVALID_REQUEST"}
+                        """))
+        );
+    }
+
+    public void stubStartAuthWith404() {
+        wiremock.stubFor(
+            WireMock.post(urlEqualTo("/auth/start"))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.NOT_FOUND_404)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"NOT_FOUND"}
+                        """))
+        );
+    }
+
+    public void stubStartAuthWithDelay() {
+        wiremock.stubFor(
+            WireMock.post(urlEqualTo("/auth/start"))
+                .willReturn(aResponse()
+                    .withFixedDelay(TIMEOUT_DELAY_MS))
+        );
+    }
+
+    public void stubAuthStatusWith400(UUID authProcessUuid) {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/auth/status/" + authProcessUuid))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.BAD_REQUEST_400)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"INVALID_REQUEST"}
+                        """))
+        );
+    }
+
+    public void stubAuthStatusWith404(UUID authProcessUuid) {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/auth/status/" + authProcessUuid))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.NOT_FOUND_404)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"NOT_FOUND"}
+                        """))
+        );
+    }
+
+    public void stubAuthStatusWithServerError(UUID authProcessUuid) {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/auth/status/" + authProcessUuid))
+                .willReturn(serverError().withBody("""
+                    {"errorCode":"AUTH_SERVER_ERROR_CODE"}
+                    """))
+        );
+    }
+
+    public void stubAuthStatusWithDelay(UUID authProcessUuid) {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/auth/status/" + authProcessUuid))
+                .willReturn(aResponse()
+                    .withFixedDelay(TIMEOUT_DELAY_MS))
+        );
+    }
+
+    public void stubWellKnownWith400() {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/.well-known/jwks.jws"))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.BAD_REQUEST_400)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"INVALID_REQUEST"}
+                        """))
+        );
+    }
+
+    public void stubWellKnownWith404() {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/.well-known/jwks.jws"))
+                .willReturn(aResponse()
+                    .withStatus(HttpStatus.NOT_FOUND_404)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("""
+                        {"errorCode":"NOT_FOUND"}
+                        """))
+        );
+    }
+
+    public void stubWellKnownWithServerError() {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/.well-known/jwks.jws"))
+                .willReturn(serverError().withBody("""
+                    {"errorCode":"AUTH_SERVER_ERROR_CODE"}
+                    """))
+        );
+    }
+
+    public void stubWellKnownWithDelay() {
+        wiremock.stubFor(
+            WireMock.get(urlEqualTo("/.well-known/jwks.jws"))
+                .willReturn(aResponse()
+                    .withFixedDelay(TIMEOUT_DELAY_MS))
         );
     }
 
